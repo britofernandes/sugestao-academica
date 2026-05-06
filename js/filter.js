0d98516f85daa0f0a4d4f6702c9b03dff03b1f53
britofernandes <brito.fernandes@aluno.ifsp.edu.br>
2026-05-05 22:20:09 -0300

    algumas mudanças

---
+++ js/filter.js
@@ -0,0 +1,12 @@
+export const filter = [
+    { name: "Biologia", color: "#990000" },
+    { name: "Física", color: "#330000" },
+    { name: "Gestão", color: "#cc9999" },
+    { name: "Informática", color: "#ffccff" },
+    { name: "Letras", color: "#999933" },
+    { name: "Pedagogia", color: "#00cc99" },
+    { name: "Sociologia", color: "#0033ff" },
+    { name: "Técnico Administrativo", color: "#000000" },
+    { name: "Geografia", color: "#330099" },
+    { name: "Artes", color: "#000099" }
+];
